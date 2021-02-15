e778afebf849891c6963fa94584f89a390b1b4a2
a060110 <a060110@gmail.com>
2021-02-15 16:00:17 +0800

    homework finished

--- ClassLibrary4/Class2.cs
+++ ClassLibrary4/Class2.cs
@@ -11,13 +11,17 @@ namespace ClassLibrary4
     {
         public static string SentenceMethod1(string p1)
         {
-            string[] Sentence = p1.Split('\r', '\n');
-            var AllSentence = new System.Text.StringBuilder();
-            for (int i = 0; i <=2; i++)
+            if (string.IsNullOrEmpty(p1))
             {
-                AllSentence.AppendLine(Sentence[i]+"<br>");
+                return "";
             }
-            return AllSentence.ToString();
+                string[] Sentence = p1.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                var AllSentence = new System.Text.StringBuilder();
+                foreach (string i in Sentence)
+                {
+                    AllSentence.AppendLine(i + "<br>");
+                }
+                return AllSentence.ToString();
         }
     }
 
@@ -28,6 +32,18 @@ namespace ClassLibrary4
         {
             string expect = "我叫賴群凱<br>\r\n正在學C#<br>\r\n請多多指教<br>\r\n";
             Assert.AreEqual(expect, SentenceClass1.SentenceMethod1("我叫賴群凱\r\n正在學C#\r\n請多多指教"));
+        }
+        [Test]
+        public static void TestMothodFourLine()
+        {
+            string expect= "我叫賴群凱<br>\r\n正在學C#<br>\r\n請多多指教<br>\r\n謝謝<br>\r\n";
+            Assert.AreEqual(expect, SentenceClass1.SentenceMethod1("我叫賴群凱\r\n正在學C#\r\n請多多指教\r\n謝謝"));
+        }
+        [Test]
+        public static void TestNull()
+        {
+            string expect = "";
+            Assert.AreEqual(expect, SentenceClass1.SentenceMethod1(null));
         }
     }
 }
